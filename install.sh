80e199aab33e49896086bb6d67385b60e72e8217
tau-zeta <taufiqhaikal1808@gmail.com>
2026-04-27 12:33:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p vim git --run "bash ./install.sh"
 
-git clone https://github.com/unlucky-shen/.FlakeBtw.git
+git clone https://github.com/tau-zeta/.flake.git
 git clone https://github.com/unlucky-shen/hypr.git
 git clone https://github.com/unlucky-shen/kitty.git
 
